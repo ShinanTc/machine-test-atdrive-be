bf23b65e741cbb666177890cba8e9512cc2b7ba8
Shinan Tc <shinantc1@gmail.com>
2025-11-15 15:20:22 +0530

    dbconfig seperated

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
 const productRoutes = require('./routes/productRoutes');
+const connectDB = require('./config/db');
 
 const app = express();
 
@@ -11,17 +11,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection
-const MONGODB_URL = process.env.MONGODB_URL;
-
-mongoose.connect(MONGODB_URL)
-    .then(() => console.log('MongoDB connected successfully'))
-    .catch(error => console.error('MongoDB connection error:', error));
+// Connect to MongoDB
+connectDB();
 
 // Base Routes
 app.use('/api/products', productRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
